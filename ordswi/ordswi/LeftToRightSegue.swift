54e8d2dade3560295c961363ea75ab525b07d798
matulik <matulik>
2015-07-06 16:03:33 +0200

    TODO Fix exception!

---
+++ ordswi/ordswi/LeftToRightSegue.swift
@@ -0,0 +1,13 @@
+//
+//  LeftToRightSegue.swift
+//  ordswi
+//
+//  Created by CS_praktykant on 06/07/15.
+//  Copyright (c) 2015 mt. All rights reserved.
+//
+
+import UIKit
+
+class LeftToRightSegue: UIStoryboardSegue {
+   
+}
